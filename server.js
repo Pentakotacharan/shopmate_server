e2688da11ef3edb7e9e67b1edfb5933d0852de71
Pentakotacharan <charanpentakota2006@gmail.com>
2026-01-24 16:55:26 +0530

    added

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(
 );
 // // Middleware
 app.use(cors({
-    origin: ["https://shopmate-frontend.vercel.app", "http://localhost:5173", "http://localhost:3000"], 
+    origin: ["https://shopmate-client.vercel.app", "http://localhost:5173", "http://localhost:3000"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
